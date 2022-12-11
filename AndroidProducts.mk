a439322a516fbb58a1e20d3a7676a8b42a814ce4
galang8664 <95033122+galang8664@users.noreply.github.com>
2022-12-11 07:22:00 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/lineage_lava.mk
+   $(LOCAL_DIR)/cipher_lava.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lava-user \
-    lineage_lava-userdebug \
-    lineage_lava-eng
+    cipher_lava-user \
+    cipher_lava-userdebug \
+    cipher_lava-eng
